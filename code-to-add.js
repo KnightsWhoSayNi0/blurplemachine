8d59f58129d1bee8d3c20e2652af89c867da03a8
KnightsWhoSayNi <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2021-06-23 12:03:19 -0400

    Create code-to-add.js

---
+++ code-to-add.js
@@ -0,0 +1,56 @@
+/*
+  BLURPLE MACHINE
+*/
+
+function revert(){
+	document.body.style.setProperty('--brand-experiment-100', '#f8f9fd');
+	document.body.style.setProperty('--brand-experiment-130', '#f2f4fc');
+	document.body.style.setProperty('--brand-experiment-160', '#ebeefa');
+	document.body.style.setProperty('--brand-experiment-200', '#e3e7f8');
+	document.body.style.setProperty('--brand-experiment-230', '#dae0f5');
+	document.body.style.setProperty('--brand-experiment-260', '#d1d9f3');
+	document.body.style.setProperty('--brand-experiment-300', '#c7d0f0');
+	document.body.style.setProperty('--brand-experiment-330', '#b5c1ec');
+	document.body.style.setProperty('--brand-experiment-360', '#a5b3e7');
+	document.body.style.setProperty('--brand-experiment-400', '#8ea1e1');
+	document.body.style.setProperty('--brand-experiment-430', '#869adf');
+	document.body.style.setProperty('--brand-experiment-460', '#7d92dd');
+	document.body.style.setProperty('--brand-experiment', '#7289da');
+	document.body.style.setProperty('--brand-experiment-500', '#7289da');
+	document.body.style.setProperty('--brand-experiment-530', '#687dc6');
+	document.body.style.setProperty('--brand-experiment-560', '#5c6fb1');
+	document.body.style.setProperty('--brand-experiment-600', '#4e5d94');
+	document.body.style.setProperty('--brand-experiment-630', '#435180');
+	document.body.style.setProperty('--brand-experiment-660', '#3b4770');
+	document.body.style.setProperty('--brand-experiment-700', '#2e3757');
+	document.body.style.setProperty('--brand-experiment-730', '#2b3352');
+	document.body.style.setProperty('--brand-experiment-760', '#272f4b');
+	document.body.style.setProperty('--brand-experiment-800', '#222941');
+	document.body.style.setProperty('--brand-experiment-830', '#1a2032');
+	document.body.style.setProperty('--brand-experiment-860', '#111521');
+	document.body.style.setProperty('--brand-experiment-900', '#06070b');
+	document.body.style.setProperty('--brand-experiment-05a', 'rgba(114, 137, 218, 0.05)');
+	document.body.style.setProperty('--brand-experiment-10a', 'rgba(114, 137, 218, 0.1)');
+	document.body.style.setProperty('--brand-experiment-15a', 'rgba(114, 137, 218, 0.15)');
+	document.body.style.setProperty('--brand-experiment-20a', 'rgba(114, 137, 218, 0.2)');
+	document.body.style.setProperty('--brand-experiment-25a', 'rgba(114, 137, 218, 0.25)');
+	document.body.style.setProperty('--brand-experiment-30a', 'rgba(114, 137, 218, 0.3)');
+	document.body.style.setProperty('--brand-experiment-35a', 'rgba(114, 137, 218, 0.35)');
+	document.body.style.setProperty('--brand-experiment-40a', 'rgba(114, 137, 218, 0.4)');
+	document.body.style.setProperty('--brand-experiment-45a', 'rgba(114, 137, 218, 0.45)');
+	document.body.style.setProperty('--brand-experiment-50a', 'rgba(114, 137, 218, 0.5)');
+	document.body.style.setProperty('--brand-experiment-55a', 'rgba(114, 137, 218, 0.55)');
+	document.body.style.setProperty('--brand-experiment-60a', 'rgba(114, 137, 218, 0.6)');
+	document.body.style.setProperty('--brand-experiment-65a', 'rgba(114, 137, 218, 0.65)');
+	document.body.style.setProperty('--brand-experiment-70a', 'rgba(114, 137, 218, 0.7)');
+	document.body.style.setProperty('--brand-experiment-75a', 'rgba(114, 137, 218, 0.75)');
+	document.body.style.setProperty('--brand-experiment-80a', 'rgba(114, 137, 218, 0.8)');
+	document.body.style.setProperty('--brand-experiment-85a', 'rgba(114, 137, 218, 0.85)');
+	document.body.style.setProperty('--brand-experiment-90a', 'rgba(114, 137, 218, 0.9)');
+	document.body.style.setProperty('--brand-experiment-95a', 'rgba(114, 137, 218, 0.95');
+	document.body.style.setProperty('--font-display', 'Whitney, "Helvetica Neue", Helvetica, Arial, sans-serif');
+	document.body.style.setProperty('--text-positive', '#43b581');
+}
+
+console.log('[blurple machine] trying to revert colors')
+revert();
